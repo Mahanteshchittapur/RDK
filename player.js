00b2a298456460aee13da2529cbcd8e55b249c26
Mahanteshchittapur <101130254+Mahanteshchittapur@users.noreply.github.com>
2023-08-21 10:49:49 +0530

    Update player.js

--- player.js
+++ player.js
@@ -7,10 +7,10 @@ var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices
     
 
     function loadAAMPVideo() {
-        //window.onload = function() {
+        window.onload = function() {
         var video = document.getElementById("Video");
         video.src = url;
-        // video.style.display = "block";
+        video.style.display = "block";
         video.play();
     
     }
